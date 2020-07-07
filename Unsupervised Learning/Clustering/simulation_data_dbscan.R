b61e8b26dc6c11db79d85972446543248cb1981a
JunHyun-DS <60165706+JunHyun-DS@users.noreply.github.com>
2020-07-07 23:18:51 +0900

    Add files via upload

---
+++ Unsupervised Learning/Clustering/simulation_data_dbscan.R
@@ -0,0 +1,18 @@
+setwd('D:\\R')
+source('simulation8.R')
+library(fpc) # dbscan
+library(fossil) # rand index
+
+simulation_data = simulation_data # simulation8 data
+
+dbs=dbscan(simulation_data, eps=.4, MinPts = 4) # DBSCAN 알고리즘
+
+table(dbs$cluster)
+
+# clustering 후 시각화
+plot(simulation_data, col=dbs$cluster)
+
+# rand index: clustering 성능평가 지표 
+randIndex = rand.index(test_cluster, dbs$cluster)
+
+cat('banana data에서 dbscan의 randIndex는 ', randIndex, '입니다.', sep='')
